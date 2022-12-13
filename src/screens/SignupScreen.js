b070ba5a1197b323e0855af1554a0fa9cf6e4ae5
Venkatesh Ranjan Verma <72699033+VenkateshRanjanVerma@users.noreply.github.com>
2022-12-13 23:17:57 +0530

    changes done

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -106,7 +106,7 @@ const styles = StyleSheet.create({
     centeredView: {
       flex: 1,
       alignItems: 'center',
-      justifyContent: 'flex-end'
+      justifyContent: 'center'
     },
     container: {
       ...StyleSheet.absoluteFillObject,
